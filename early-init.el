d6ce44b9de900542ae95f406cb1a9e2a5e2dc15a
Wim Alsemgeest <wim@wimalsemgeest.nl>
2023-06-10 23:31:17 +0200

    Added some packages

--- early-init.el
+++
@@ -1,4 +0,0 @@
-;; Add some padding in Emacs frames
-(dolist (var '(default-frame-alist initial-frame-alist))
-  (add-to-list var '(right-divider-width . 20))
-  (add-to-list var '(internal-border-width . 20)))
