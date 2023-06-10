1d6fecc9282e73f61f5e2d045c3538b77e71dd37
Wim Alsemgeest <wim@wimalsemgeest.nl>
2023-06-10 11:42:18 +0200

    Basic setup for Emacs

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
-;; ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
 ;;    Emacs configuration
 ;;    Wim Alsemgeest
-;; ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
 ;;    History:
 ;;             - Started the configuration on 10-06-2023
-;; ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
 
-;; ----- General settings ----------------------------------------
+;; ----- General settings -----------------------------------------------------
 (setq inhibit-startup-message t)       ;; Don't show the splash screen
 (setq visible-bell t)                  ;; Flash when the bell rings
 (tool-bar-mode -1)                     ;; Turn off the toolbar
@@ -14,8 +14,18 @@
 ;; (menu-bar-mode -1)                     ;; Turn off the menubar
 (global-display-line-numbers-mode 1)   ;; Show linenumbers
 (setq column-number-mode t)            ;; Show column numbers
+(recentf-mode 1)                       ;; Remember the last edited files
+(setq history-length 25)               ;; Remember the last minibuffer prompts
+(savehist-mode 1)                      ;; Enable save-history mode
+(save-place-mode 1)                    ;; Remember and restore de last cursor position
+(setq use-dialog-box nil)              ;; Prevent using UI dialog for prompts
+(global-auto-revert-mode 1)            ;; Revert buffers when file is changed
+(setq global-auto-revert-non-file-buffers t)  ;; Revert Dired and other buffers
 
 
+;; ----- Move customization variables to a separate file and load it ----------
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
 
 ;; ----- Theme ----------------------------------------------------------------
 ;;   Theme configuration before loading the theme
